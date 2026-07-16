ab151780fa48c7bcae8a1119882fcc40985cece8
samrisin <63247765+samrisin@users.noreply.github.com>
2026-07-16 14:14:53 -0700

    Add: cheapest-flight-path - submission-2

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        for(int i=0;i<=k;i++){
+             int[] temp = Arrays.copyOf(dist, n);
+            for(int[] flight:flights){
+                int u = flight[0];
+                int v = flight[1];
+                int w = flight[2];
+                if(dist[u]!=Integer.MAX_VALUE && temp[v]>dist[u]+w){
+                    temp[v] = dist[u]+w;
+                }
+            }
+            dist = temp;
+        }
+
+    return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
+    }
+}
