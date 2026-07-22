a14db2cdd0ea72cf4eb706ad3a1b35a71051aee2
samrisin <63247765+samrisin@users.noreply.github.com>
2026-07-22 10:12:56 -0700

    Add: house-robber-ii - submission-9

---
+++ Data Structures & Algorithms/house-robber-ii/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+
+        int rob1 = maxRob(Arrays.copyOfRange(nums,0,n-1));
+        int rob2  = maxRob(Arrays.copyOfRange(nums,1,n));
+
+        return Math.max(rob1,rob2);
+    }
+
+
+    public int maxRob(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+    return dp[n-1];
+    }
+}
