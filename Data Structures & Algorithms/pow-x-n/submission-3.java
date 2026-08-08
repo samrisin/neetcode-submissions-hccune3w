4526114385be57ae9955695bee44f819c31f8025
samrisin <63247765+samrisin@users.noreply.github.com>
2026-08-07 17:43:57 -0700

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        long power = n;
+        if (n < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        double result = 1;
+        while (power > 0) {
+            if (power % 2 == 1) result *= x;
+            x *= x;
+            power /= 2;
+        }
+        return result;
+    }
+}
